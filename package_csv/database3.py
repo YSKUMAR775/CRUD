d3437e159477ed4e539f94524323f392e67823f9
YS Kumar <70428965+YSKUMAR775@users.noreply.github.com>
2020-12-19 23:47:46 +0530

    Update database3.py

--- package_csv/database3.py
+++ package_csv/database3.py
@@ -2,8 +2,8 @@ import pymysql
 
 
 def db_connect3():
-    db = pymysql.connect(host='localhost',
-                         user='root',
-                         password='S@i30051995',
+    db = pymysql.connect(host='aws2.c42ojr1a1cpj.ap-south-1.rds.amazonaws.com',
+                         user='admin',
+                         password='yskumar775',
                          db='crud')
     return db
